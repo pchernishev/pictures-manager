6162701ea3504c9746322ee2e0d3c2983091244c
Pavel Chernishev <pchernishev@gmail.com>
2016-12-03 22:52:13 +0000

    removed mode and started changing code to work without pattern matching names only

--- logger.py
+++ logger.py
@@ -2,14 +2,16 @@ import logging
 
 
 def init_logger():
-    logging.basicConfig(filename='logger.log', level=logging.DEBUG, format='%(asctime)s %(message)s',
-                        datefmt='%d/%m/%Y %H:%M:%S')
+    _format = '%(asctime)s - %(levelname)s - %(filename)s:%(funcName)s:%(lineno)d - %(message)s'
+    date_format = '%d/%m/%Y %H:%M:%S'
+    logging.basicConfig(filename='logger.log', level=logging.DEBUG, format=_format,
+                        datefmt=date_format)
     logger = logging.getLogger('logger')
     # create console handler and set level to debug
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
     # create formatter
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter(_format, date_format)
     # add formatter to ch
     ch.setFormatter(formatter)
     # add ch to logger
--- regex_patterns.py
+++ regex_patterns.py
@@ -1,24 +1,23 @@
 NEW_FILE_FORMAT = '{}_{}.{extension}'
 NEW_SUFFIX_FORMAT = '{:0=3d}'
 DELIMITER = '[-|_| |\.|~]?'
+EXTENSIONS = ['jpg', 'jpeg', 'png', 'mp4', 'mov', 'nef', 'gif']
 REGEX_PARTS = {
-        'extension': '\.(?P<extension>jpg|jpeg|png|mp4|JPG|MOV|NEF)',
-        'date': '(?P<year>20[0-1]\d)' + DELIMITER + '(?P<month>[0-1]\d)' + DELIMITER + '(?P<day>[0-3]\d)',
+        'extension': '\.(?P<extension>{})'.format('|'.join(EXTENSIONS + [ext.upper() for ext in EXTENSIONS])),
+        'date': '((?P<year>[1-2][9|0]\d{2})' + DELIMITER + '(?P<month>[0-1]\d)' + DELIMITER + '(?P<day>[0-3]\d))',
         'time_no_milli': '(?P<hour>[0-2]\d)' + DELIMITER + '(?P<minute>\d{2})' + DELIMITER + '(?P<second>\d{2})',
         'time': '((?P<hour>[0-2]\d)' + DELIMITER + '(?P<minute>\d{2})' + DELIMITER + '(?P<second>\d{2})' +
-                DELIMITER + '(?P<millisecond>\d{3})?)?',
+                DELIMITER + '(?P<millisecond>\d{3})?)',
         'prefix': '(?P<prefix>IMG|VID|Screenshot|C360|MPC|DSC|RKP|MVI)',
-        'suffix': '(?P<suffix>\d{1,4}|\(\d\)|HDR|Burst\d{2}|WA\d{4})',
+        'suffix': '(?P<suffix>\d{1,4}|\(\d\)|\w{1,10}|Burst\d{2}|WA\d{4})',
         'delimiter': DELIMITER,
     }
 DATE_TAKEN_REGEX = '(?P<year>20[0-1]\d):(?P<month>[0-1]\d):(?P<day>[0-3]\d)[ ]' \
                    '(?P<hour>[0-2]\d):(?P<minute>\d{2}):(?P<second>\d{2})'
-ACCEPTABLE_REGEX_DICT = {
-        'phone': '^{prefix}?{delimiter}{date}{delimiter}{time}{delimiter}{suffix}?{extension}$'.format(**REGEX_PARTS),
-        'camera': '^{prefix}{delimiter}{suffix}{extension}$'.format(**REGEX_PARTS),
-}
+ACCEPTABLE_REGEX_DICT = '^{prefix}?{delimiter}{date}?{delimiter}{time}?{delimiter}{suffix}?{extension}$'.format(
+    **REGEX_PARTS)
+# '^{prefix}{delimiter}{suffix}{extension}$'.format(**REGEX_PARTS),
 DESTINATION_REGEX = '^{date}_{time_no_milli}_{suffix}{extension}'.format(**REGEX_PARTS)
-# DESTINATION_REGEX_1 = '^{date}_{time_no_milli}{suffix}{extension_1}'.format(**REGEX_PARTS)
 DESTINATION_FORMAT_NO_SUFFIX_NO_EXTENSION = '{year}{month}{day}_{hour}{minute}{second}'
 DESTINATION_FORMAT_NO_SUFFIX = DESTINATION_FORMAT_NO_SUFFIX_NO_EXTENSION + '.{extension}'
 DESTINATION_FORMAT = DESTINATION_FORMAT_NO_SUFFIX_NO_EXTENSION + '{suffix}' + '{extension}'
